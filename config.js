9b32c7a6a61c32520c2750ccecaf81691c0d4006
so-si <40689019+so-si@users.noreply.github.com>
2026-09-15 04:08:31 +0900

    Update GAS API deployment URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.MATH_SPEED_CONFIG = {
-  apiUrl: 'https://script.google.com/macros/s/AKfycbwtnI1WanUg9X4Q8aJRv1M5qAWxlrB2WMzKl8I_Bt-k2urTa2y5zIChPY0azT2lOtI/exec',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbxaH7UesXEkw9HMcuoiv5j1knakn96OtILmGlFLy0t_-VF1mspdz_znwazkubzfLyOT/exec',
   singleStudentMode: true
 };

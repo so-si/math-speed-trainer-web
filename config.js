eaa98b99e8add3b5a0eab415edc6d075cbe63589
so-si <40689019+so-si@users.noreply.github.com>
2026-09-15 03:08:33 +0900

    Add Math Speed API config

---
+++ config.js
@@ -0,0 +1,4 @@
+window.MATH_SPEED_CONFIG = {
+  apiUrl: 'https://script.google.com/macros/s/AKfycbxaH7UesXEkw9HMcuoiv5j1knakn96OtILmGlFLy0t_-VF1mspdz_znwazkubzfLyOT/exec',
+  singleStudentMode: true
+};
